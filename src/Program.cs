515369e94ba4f492ce7a62fe2aa1e63b7ce48a01
ytyaru <yttry0@gmail.com>
2020-10-28 14:11:37 +0900

    parsed value

--- src/Program.cs
+++ src/Program.cs
@@ -14,8 +14,19 @@ namespace TsvToHtmlTable
             sub.Method();
 
             Parser.Default.ParseArguments<Options>(args)
-                .WithParsed(opt => {/*パースに成功した場合*/})
+                .WithParsed(opt => {/*パースに成功した場合*/ Run(opt); })
                 .WithNotParsed(er => {/*パースに失敗した場合*/});
+        }
+        private static void Run(Options opt)
+        {
+            Console.WriteLine("CommandLine parse success!!");
+//            Console.WriteLine(opt.A);
+            Console.WriteLine($"A: {opt.A}");
+            Console.WriteLine($"B: {opt.B}");
+            Console.WriteLine($"C: {string.Join(", ", opt.C)}");
+            Console.WriteLine($"D: {opt.D}");
+            Console.WriteLine($"1: {string.Join(", ", opt.Remaining)}");
+
         }
     }
 }
