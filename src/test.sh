6a08158bcd78db1ef9aa0203bd2b65ef835ef1d3
ytyaru <ytyaru@outlook.jp>
2020-11-02 11:49:46 +0900

    mod test.sh

--- src/test.sh
+++ src/test.sh
@@ -8,13 +8,14 @@ Run() {
 	THIS="$(realpath "${BASH_SOURCE:-0}")"; HERE="$(dirname "$THIS")"; PARENT="$(dirname "$HERE")"; THIS_NAME="$(basename "$THIS")"; APP_ROOT="$PARENT";
 	. "$HERE/install.sh"
 	. "$HERE/run.sh"
-
 	cd "$APP_ROOT"
 	TSV="./res/tsv"
 	CMD="./bin"
 	echo "========== TEST =========="
 	cat $TSV/matrix_3.tsv | $CMD/tsv2table --version
+	cat $TSV/matrix_3.tsv | $CMD/tsv2table version
 	cat $TSV/matrix_3.tsv | $CMD/tsv2table --help
+	cat $TSV/matrix_3.tsv | $CMD/tsv2table help
 	cat $TSV/matrix_3.tsv | $CMD/tsv2table g --help
 	cat $TSV/matrix_3.tsv | $CMD/tsv2table i --help
 	cat $TSV/matrix_3.tsv | $CMD/tsv2table n --help
@@ -52,12 +53,16 @@ Run() {
 	cat $TSV/row_1.tsv | $CMD/tsv2table -r B
 	set +x
 	echo "----- inner-header -----"
+	set -x
 	cat $TSV/matrix_3.tsv | $CMD/tsv2table i
 	cat $TSV/matrix_3.tsv | $CMD/tsv2table i -H r
 	cat $TSV/matrix_3.tsv | $CMD/tsv2table i -s 2
 	cat $TSV/matrix_3.tsv | $CMD/tsv2table i -S 2
 	cat $TSV/matrix_3.tsv | $CMD/tsv2table i -s 2 -S 3
+	set +x
 	echo "----- none-header -----"
+	set -x
 	cat $TSV/matrix_3.tsv | $CMD/tsv2table n
+	set +x
 }
 Run "$@"
