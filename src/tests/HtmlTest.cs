affce1b467b867f2815bbb7fa8295172152b51c2
ytyaru <ytyaru@outlook.jp>
2020-11-05 13:02:40 +0900

    add TestMakeAttrsString(), TestMakeAttrsList()

--- src/tests/HtmlTest.cs
+++ src/tests/HtmlTest.cs
@@ -76,6 +76,34 @@ namespace TsvToHtmlTable.Tests
             new object[] { new KeyValuePair<string,string>("", "tsv2table"), ""}
         };
 
+        [TestCaseSourceAttribute("MakeAttrsStringCases")]
+        public void TestMakeAttrsString(string[] keyvalues, string expected)
+        {
+            Assert.AreEqual(expected, Html.MakeAttrs(keyvalues));
+        }
+        private static readonly object[] MakeAttrsStringCases = {
+            new object[] { new string[] { "", "" }, ""},
+            new object[] { new string[] { null!, null! }, ""},
+            new object[] { new string[] { "", null! }, ""},
+            new object[] { new string[] { null!, "" }, ""},
+            new object[] { new string[] { "class", "tsv2table" }, "class=\"tsv2table\""},
+            new object[] { new string[] { "class", "" }, ""},
+            new object[] { new string[] { "", "tsv2table" }, ""},
+        };
+        [TestCaseSourceAttribute("MakeAttrsListCases")]
+        public void TestMakeAttrsList(List<KeyValuePair<string,string>> pairs, string expected)
+        {
+            Assert.AreEqual(expected, Html.MakeAttrs(pairs));
+        }
+        private static readonly object[] MakeAttrsListCases = {
+            new object[] { new List<KeyValuePair<string,string>> { new KeyValuePair<string,string>("","") }, ""},
+            new object[] { new List<KeyValuePair<string,string>> { new KeyValuePair<string,string>(null!,null!) }, ""},
+            new object[] { new List<KeyValuePair<string,string>> { new KeyValuePair<string,string>("",null!) }, ""},
+            new object[] { new List<KeyValuePair<string,string>> { new KeyValuePair<string,string>(null!,"") }, ""},
+            new object[] { new List<KeyValuePair<string,string>> { new KeyValuePair<string,string>("class","tsv2table") }, "class=\"tsv2table\""},
+            new object[] { new List<KeyValuePair<string,string>> { new KeyValuePair<string,string>("class","") }, ""},
+            new object[] { new List<KeyValuePair<string,string>> { new KeyValuePair<string,string>("","tsv2table") }, ""}
+        };
         //[TestCase(KeyValuePair<string,string>{Key="",Value=""}, "")]
     }
 }
